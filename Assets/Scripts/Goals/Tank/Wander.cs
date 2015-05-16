a5ff2e409e7c36753b8b38ed9afa253b7adc22a3
Luuk Holleman <luukholleman@gmail.com>
2015-05-16 15:29:31 +0200

    Added wander goal

---
+++ Assets/Scripts/Goals/Tank/Wander.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Goals.Tank
+{
+    class Wander : Goal
+    {
+        private SteeringBehaviour _steeringBehaviour;
+
+        public override void Activate()
+        {
+            _steeringBehaviour = SteeringBehaviour.CreateInstance<SteeringBehaviour>();
+
+            _steeringBehaviour.SetGameObject(Instance);
+        }
+
+        public override STATUS Process()
+        {
+            Instance.GetComponent<Rigidbody2D>().AddForce(_steeringBehaviour.Wander());
+
+            return SetStatus(STATUS.ACTIVE);
+        }
+
+        public override void Terminate()
+        {
+            
+        }
+
+        public override bool HandleMessage()
+        {
+            return true;
+        }
+    }
+}
