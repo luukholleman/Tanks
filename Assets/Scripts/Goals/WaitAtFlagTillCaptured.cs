df9d4a799807ba58de7ae7f8423048f99ff560ee
Luuk Holleman <luukholleman@gmail.com>
2015-06-01 10:17:24 +0200

    Remove obsolete code

--- Assets/Scripts/Goals/WaitAtFlagTillCaptured.cs
+++ Assets/Scripts/Goals/WaitAtFlagTillCaptured.cs
@@ -31,21 +31,7 @@ namespace Assets.Scripts.Goals
         public override STATUS Process()
         {
             _rigidbody.AddForce(_steeringBehaviour.Stop(_rigidbody.velocity, 0.1f));
-
-            if (_flag.GetComponent<Flag>().Side == Instance.GetComponent<Vehicle>().Side && Math.Abs(_flag.GetComponent<Flag>().Score) == _flag.GetComponent<Flag>().MaxScore)
-            {
-                return SetStatus(STATUS.COMPLETED);
-            }
-
-            if (Vector2.Distance(Instance.transform.position, _flag.transform.position) > _flag.GetComponent<Flag>().CappingRange)
-            {
-                // somehow we got out of the range of the flag (probably shot out), we need to get back in range
-                if ((SubGoals.Any() && SubGoals.Peek().GetType() != typeof(CaptureFlag)) || !SubGoals.Any())
-                    AddSubGoal(new CaptureFlag(_flag));
-
-                return ProcessSubGoals();
-            }
-
+            
             return SetStatus(STATUS.ACTIVE);
         }
 
