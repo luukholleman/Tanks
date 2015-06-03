f3714af1da480949586b24374b5965e46594f40e
Luuk Holleman <luukholleman@gmail.com>
2015-06-03 22:22:15 +0200

    Optimizations

--- Assets/Scripts/Pathfinding/AStar.cs
+++ Assets/Scripts/Pathfinding/AStar.cs
@@ -14,59 +14,58 @@ namespace Assets.Scripts.Pathfinding
 
         public List<GraphNode> Path = new List<GraphNode>();
 
-        private int _source;
+        private readonly int _target;
 
-        private int _target;
+        private readonly int[] _previous;
+        private readonly float[] _distances;
 
-        Dictionary<int, int> _previous = new Dictionary<int, int>();
-        Dictionary<int, float> _distances = new Dictionary<int, float>();
         public List<int> Closed = new List<int>();
-        //PriorityQueue<int> pq = new PriorityQueue<int>();
+        //PriorityQueue<int> Open = new PriorityQueue<int>();
 
-        PriorityQueue pq = new PriorityQueue();
+        PriorityQueue Open = new PriorityQueue();
 
-        int i = 0;
+        int _iterationCount;
 
-        private int _iterationsPerCall = 1000;
+        private int _iterationsPerCall = 20;
 
         Stopwatch sw = new Stopwatch();
         public AStar(Graph graph, int source, int target)
         {
             _graph = graph;
-            _source = source;
             _target = target;
+
+            _previous = new int[_graph.NodeCount];
+            _distances = new float[_graph.NodeCount];
 
             foreach (var vertex in _graph.GetNodes())
             {
-                if (vertex.Index == _source)
-                {
+                if (vertex.Index == source)
                     _distances[vertex.Index] = 0;
-                }
                 else
-                {
                     _distances[vertex.Index] = float.MaxValue;
-                }
             }
 
-            pq.Enqueue(_source, 0);
+            // queue starting node
+            Open.Enqueue(source, 0);
         }
 
         public bool Search()
         {
-            int j = 0;
+            int currentIterations = 0;
 
             sw.Start();
 
-            while (!pq.IsEmpty())
+            while (!Open.IsEmpty())
             {
-                i++;
+                _iterationCount++;
 
-                var smallest = (int)pq.Dequeue();
+                var smallest = (int)Open.Dequeue();
 
                 // we've found the path
                 if (smallest == _target)
                 {
-                    while (_previous.ContainsKey(smallest))
+                    // create path as list
+                    while (_previous[smallest] != 0)
                     {
                         Path.Add(_graph.GetNode(smallest));
                         smallest = _previous[smallest];
@@ -74,10 +73,11 @@ namespace Assets.Scripts.Pathfinding
 
                     sw.Stop();
 
-                    Debug.Log("Path found!!");
-                    Debug.Log("Iterations: " + i);
-                    Debug.Log(sw.Elapsed);
+                    //Debug.Log("Path found!!");
+                    //Debug.Log("Iterations: " + _iterationCount);
+                    //Debug.Log(sw.Elapsed);
 
+                    // path is reversed
                     Path.Reverse();
 
                     return true;
@@ -91,116 +91,36 @@ namespace Assets.Scripts.Pathfinding
 
                 foreach (var neighbor in _graph.GetNode(smallest).Edges)
                 {
-                    var alternativeCost = _distances[smallest] + neighbor.Cost;
+                    var neighbourCost = _distances[smallest] + neighbor.Cost;
 
-                    if (alternativeCost < _distances[neighbor.To])
+                    // is this current path faster than a previous found path?
+                    if (neighbourCost < _distances[neighbor.To])
                     {
-                        _distances[neighbor.To] = alternativeCost;
+                        // yes it is! add the new path as solution
+                        _distances[neighbor.To] = neighbourCost;
                         _previous[neighbor.To] = smallest;
                     }
 
+                    // did we already check this node? if so, it can't be faster to check again
                     if (!Closed.Exists(n => n == neighbor.To))
                     {
+                        // calc distance to target as a* heuristic
                         float distance = Vector2.Distance(_graph.GetNode(_target).Position, _graph.GetNode(smallest).Position);
                         
-                        pq.Enqueue(neighbor.To, (int)(alternativeCost + distance));
+                        // enqueue neighbour
+                        Open.Enqueue(neighbor.To, (int)(neighbourCost + distance));
                     }
 
+                    // we've checked this node, never check it again
                     Closed.Add(neighbor.To);
                 }
 
-                if (++j >= _iterationsPerCall)
-                {
-                    return false;   
-                }
+                // limit iterations per update to increase performance
+                if (++currentIterations >= _iterationsPerCall)
+                    return false;  
             }
 
             return false;
         }
-        //private bool Search()
-        //{
-        //    Stopwatch sw = new Stopwatch();
-        //    sw.Start();
-        //    var previous = new Dictionary<int, int>();
-        //    var distances = new Dictionary<int, float>();
-        //    var nodes = new List<int>();
-        //    var Closed = new List<int>();
-
-        //    PriorityQueue<int> pq = new PriorityQueue<int>();
-
-        //    foreach (var vertex in _graph.GetNodes())
-        //    {
-        //        if (vertex.Index == _source)
-        //        {
-        //            distances[vertex.Index] = 0;
-        //        }
-        //        else
-        //        {
-        //            distances[vertex.Index] = 900001;
-        //        }
-
-        //        //nodes.Add(vertex.Index);
-        //    }
-
-        //    //nodes.Add(_graph.GetNode(_source).Index);
-
-        //    pq.Enqueue(new PriorityQueueNode<int>(0, _source));
-
-        //    int i = 0;
-        //    while (!pq.Empty())
-        //    {
-        //        i++;
-        //        //nodes.Sort((x, y) => (int)(distances[x] - distances[y]));
-
-        //        var smallest = pq.DeQueue().Value;
-
-        //        //var smallest = nodes[0];
-        //        //nodes.Remove(smallest);
-
-        //        if (smallest == _target)
-        //        {
-        //            while (previous.ContainsKey(smallest))
-        //            {
-        //                Path.Add(_graph.GetNode(smallest));
-        //                smallest = previous[smallest];
-        //            }
-
-        //            sw.Stop();
-        //            Debug.Log("Path found!!");
-        //            Debug.Log("Iterations: " + i);
-        //            Debug.Log(sw.Elapsed);
-
-        //            Path.Reverse();
-
-        //            return true;
-        //        }
-
-        //        if (distances[smallest] == 900001)
-        //        {
-        //            return false;
-        //        }
-
-        //        foreach (var neighbor in _graph.GetNode(smallest).Edges)
-        //        {
-        //            float distance = Vector2.Distance(_graph.GetNode(_target).Position,
-        //                _graph.GetNode(smallest).Position);
-
-        //            var alt = distances[smallest] + neighbor.Cost;
-
-        //            if (alt < distances[neighbor.To])
-        //            {
-        //                distances[neighbor.To] = alt;
-        //                previous[neighbor.To] = smallest;
-        //            }
-
-        //            if (!Closed.Exists(n => n == neighbor.To))
-        //                pq.Enqueue(new PriorityQueueNode<int>(alt + distance, neighbor.To));
-
-        //            Closed.Add(neighbor.To);
-        //        }
-        //    }
-
-        //    return false;
-        //}
     }
 }
